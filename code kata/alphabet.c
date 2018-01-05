d81f35e9464ee09c3884c801bacf3ee381c63693
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-01-05 07:44:42 -0300

    Create alphabet.c

---
+++ code kata/alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+voidmain()
+{
+char ch;
+printf("enter any alphabet");
+scanf(%c",&ch);
+if(ch>=65&&ch<=90)||(ch>=97&&ch<=122))
+{
+printf("entered number is alphabet");
+}
+else
+{
+printf("entered number is not an alphabet");
+}
+getch();
+}
