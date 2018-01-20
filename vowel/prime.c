64aafe01f948416ddb11e02ac0e98029d324eb59
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-01-20 14:27:44 +0530

    Create prime.c

---
+++ vowel/prime.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{ 
+int n,i,count=0;
+printf("enter the number");
+scanf("%d",&n);
+for(i=2;i<n;i++)
+{ 
+if((n%1)==0)
+count++;
+}  
+if(count==0)
+{ 
+printf ("yes");
+}  	
+else
+{ 
+printf ("no");
+}  
+}  
