3f78e94b13c23898f02b791324ef771aa9bde262
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-01-20 15:15:52 +0530

    Create fact.c

---
+++ vowel/fact.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int num,i,fact=1;
+printf("enter the number",num);
+scanf("%d",&num);
+for(i=0;i<=num;i++)
+{
+fact=fact+i;
+}
+printf("factorial is %d",fact);
+return 0;
+}
