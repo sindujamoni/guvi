925efd03428635a89d1daf081481bbbdeac79a69
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-01-21 10:28:40 +0530

    Create arithmeticpro.c

---
+++ vowel/arithmeticpro.c
@@ -0,0 +1,20 @@
+	#include<stdio.h>
+	int main()
+	{
+		int n,a d;
+		printf("first term",a);
+		scanf("%d",&a);
+		printf("common difference",d);
+		scanf("%d",&d);
+		printf("N term",n);
+		scanf("%d",&n);
+		while(counter<=n)
+		{
+			a=1+(counter-1)*d;
+			printf("%d",a);
+			counter++;
+	}
+return 0;
+	}
+
+
