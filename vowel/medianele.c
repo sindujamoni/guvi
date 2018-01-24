d70b74b94c4ceef97a243a62f2b0fc07183d974a
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-01-24 15:45:59 +0530

    Create medianele.c

---
+++ vowel/medianele.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int e,b[10],i,sum=0;
+printf("Enter how many numbers \n");
+scanf("%d",&e);
+for(i=0;i<c;i++)
+{
+scanf("%d",&b[i]);
+sum=sum+b[i];
+}
+int median=sum/e;
+printf("median is :%d\n",median);
+}
