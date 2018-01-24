ec91828a58521c8558d5a663e431a1c005eb813d
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-01-24 15:43:43 +0530

    Update sortedarray.c

--- vowel/sortedarray.c
+++ vowel/sortedarray.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int c,a,i,j;
-scanf("%d",&c);
-int b[c];
+int r,a,i,j;
+scanf("%d",&r);
+int b[r];
 printf("\n the input of an array is");
-for(i=0;i<c;i++)
+for(i=0;i<r;i++)
 {
 scanf("%d",&b[i]);
 }
 printf("\n the output of an array is");
-for(j=0;j<c;j++)
+for(j=0;j<r;j++)
 {
 printf("%d",b[j]);
 }
