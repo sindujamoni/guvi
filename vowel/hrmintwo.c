f2e898ebea7483e91fcc6a1d13b7bbeb3fc41977
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-01-31 11:06:08 +0000

    Create hrmintwo.c

---
+++ vowel/hrmintwo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e,f;
+    printf("enter first time");
+    scanf("%d%d",&a,&b);
+    printf("enter the second time");
+    scanf("%d%d",&c,&d);
+    e=a-c;
+    f=b-d;
+{
+printf("%d%d",e,f);
+}
+return 0;
+}
