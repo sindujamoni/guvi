ad216cc4ab7592b557e5a616c4d1d913605ac7db
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-02-05 16:29:20 +0530

    Create numrange10.c

---
+++ vowel/numrange10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    if((n>=1)&&(n<=10))
+    {
+        printf("number is present");
+    }
+        else
+        {
+            printf("no number");
+        }
+        return 0;
+}
+        
