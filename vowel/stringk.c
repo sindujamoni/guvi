ec35c40df249b485c058c2f5ce99e15c7a9836c9
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-02-05 15:38:51 +0530

    Create stringk.c

---
+++ vowel/stringk.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[20];
+    int i,k;
+    scanf("%d",&k);
+    scanf("%s",&a);
+    for(i=0;i<=k;i++)
+    {
+        printf("%s",a);
+    }
+    return 0;
+}
+    
