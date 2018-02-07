f2f5fbb80fa9dcea601fbffbb1c1b95a394bb354
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-02-07 15:50:31 +0530

    Create nnum48.c

---
+++ vowel/nnum48.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a,sum=0;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("enter the number");
+        scanf("%d",&a);
+        sum=sum+a;
+    }
+    printf("avg is %d",sum/n);
+    return 0;
+}
