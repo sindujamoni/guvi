89e2e3882348aa41e3ec87cbc3ec4cde8b2c2449
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-02-07 15:39:41 +0530

    Update swapbitwise.c

--- vowel/swapbitwise.c
+++ vowel/swapbitwise.c
@@ -2,10 +2,10 @@
 int main()
 {
 int a=5,b=10;
-print("values of a&b before swapping %d%d",a,b);
-a=a+b;
-b=a-b;
-a=a-b;
-printf("values of a&b after swapping %d%d",a,a);
+print("values before swapping %d%d",a,b);
+a=a^b;
+b=a^b;
+a=a^b;
+printf("values after swapping %d%d",a,a);
 return 0;
 }
