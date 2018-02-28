715716a7ca41d2882576660d9cb0c23dced34f21
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-02-28 15:51:09 +0530

    Create prgm 61.c

---
+++ vowel/prgm 61.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char str[20];
+    int i,k;
+    printf("enter the string");
+    scanf("%s",&str[i]);
+    printf("\nenter how many characters you want to print");
+    scanf("%d",&k);
+for(i=0;i<k;i++)
+{
+printf("%c",str[i]);
+}
+return 0;
+}
