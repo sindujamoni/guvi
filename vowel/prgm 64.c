c51992afa48546f95074d64c1322d9b94ee50b58
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-02-28 16:19:20 +0530

    Create prgm 64.c

---
+++ vowel/prgm 64.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,m,p;
+printf("enter the numbers");
+scanf("%d",&n,&m);
+p=n+m;
+if(n%2==0)
+{
+    printf("even");
+}
+else
+{
+    printf("odd");
+}
+return 0;
+}
