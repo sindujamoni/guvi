1c5221cfe9c62ab1a507303e39ab220db70a604c
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-03-12 16:57:32 +0530

    Create prgm74.c

---
+++ vowel/prgm74.c
@@ -0,0 +1,20 @@
+#include<string.h>
+int main()
+{
+    char a[10];
+    int n;
+    printf("enter the string");
+    scanf("%s",&a);
+    n=strlen(a);
+    if(n%2==0)
+    {
+        a[n/2]='*';
+        a[(n/2)-1]='*';
+    }
+    else
+    {
+        a[n/2]='*';
+    }
+    printf("%s",a);
+    return 0;
+}
