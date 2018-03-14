41bec96d446b97bf69e4a4c8f9a58e16c6639ac4
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-03-14 16:28:54 +0530

    Create prgm 78.c

---
+++ vowel/prgm 78.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
+    
+    
