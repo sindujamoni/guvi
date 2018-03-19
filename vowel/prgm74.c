42ad037ea55d37b7af0cf6fecd61d740479dcbaf
sindujamoni <31506132+sindujamoni@users.noreply.github.com>
2018-03-19 15:43:34 +0530

    Update prgm74.c

--- vowel/prgm74.c
+++ vowel/prgm74.c
@@ -1,21 +1,11 @@
 #include<stdio.h>
-#include<string.h>
 int main()
 {
-    char a[10];
-    int n;
-    printf("enter the string");
-    scanf("%s",&a);
-    n=strlen(a);
-    if(n%2==0)
-    {
-        a[n/2]='*';
-        a[(n/2)-1]='*';
-    }
-    else
-    {
-        a[n/2]='*';
-    }
-    printf("%s",a);
+    float num;
+    int x;
+    printf("enter the number");
+    scanf("%f",&num);
+    x=num+0.5;
+    printf("%d",x);
     return 0;
 }
